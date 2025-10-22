7276d2b8877d4c734eb1172b0c27e9a6a6b7d21f
Obuleswar <obuleswar88@gmail.com>
2025-10-22 05:12:44 +0000

    // For Firebase JS SDK v7.20.0 and later, measurementId is optional cons

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -4,7 +4,7 @@ export const firebaseConfig = {
   authDomain: "wallet-8fdd4.firebaseapp.com",
   databaseURL: "https://wallet-8fdd4-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "wallet-8fdd4",
-  storageBucket: "wallet-8fdd4.appspot.com",
+  storageBucket: "wallet-8fdd4.firebasestorage.app",
   messagingSenderId: "622824988525",
   appId: "1:622824988525:web:71dcc82dab96e7f963c464",
   measurementId: "G-EY3J3RTQDR"
